6be6be0a2aa24e0fa8d519e7db638925ee7e1859
Ноутбук AQ <aq@mail.ru>
2023-08-18 19:48:45 +0300

    fix phone.py

--- src/phone.py
+++ src/phone.py
@@ -2,9 +2,26 @@ from src.item import Item
 
 
 class Phone(Item):
+    """Класс-наследник от класса Item"""
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        """
+        Инициализация экземпляра класса с атрибутами класса-родителя и новым атрибутом
+        "number_of_sim" для кол-ва сим-карт.
+        """
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        """Геттер для кол-ва сим-карт."""
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number):
+        """Сеттер для кол-ва сим-карт с проверкой на целое число и большее нуля"""
+        if not isinstance(number, int) or number <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.__number_of_sim = number
 
     def __repr__(self):
         return (f"{self.__class__.__name__}('{self.name}', {self.price}"
